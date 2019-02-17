82f4352b2271eac81681e3657bf81c1a879dd727
Matthew Pickering <matthewtpickering@gmail.com>
2019-02-17 16:57:59 +0000

    Make lsif-server work for Haskell files
    See https://github.com/Microsoft/vscode-lsif-extension/issues/3

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -42,6 +42,7 @@ export function activate(context: ExtensionContext) {
 		// Register the server for plain text documents
 		documentSelector: [
 			{ scheme: 'file', language: 'typescript', exclusive: true  }  as DocumentFilter,
+			{ scheme: 'file', language: 'haskell', exclusive: true  }  as DocumentFilter,
 			{ scheme: 'file', language: 'javascript', exclusive: true  }  as DocumentFilter,
 		],
 		synchronize: {
